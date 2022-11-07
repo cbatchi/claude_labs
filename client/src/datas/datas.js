d410a6f9152ae83f8c08dcf7fd3d5d40fc4f4daa
claudius <claude.batchi@yahoo.fr>
2022-11-07 17:22:56 +0100

    featuredProperty components finshed

--- client/src/datas/datas.js
+++ client/src/datas/datas.js
@@ -1,30 +1,146 @@
 import { FaBed, FaCar, FaPlane, FaTaxi } from "react-icons/fa"
+import { BiDrink, BiGlobe } from "react-icons/bi"
 
 export const headerIcons = [
   {
-    name: "stays",
+    name: "Hébergement",
     icon: <FaBed />,
   },
   {
-    name: "flights",
+    name: "vols",
     icon: <FaPlane />,
   },
   {
-    name: "car rentals",
+    name: "vols + hôtels",
+    icon: <BiGlobe />,
+  },
+  {
+    name: "location",
     icon: <FaCar />,
   },
   {
     name: "attractions",
-    icon: <FaBed />,
+    icon: <BiDrink />,
   },
   {
     name: "Airport taxis",
     icon: <FaTaxi />,
   },
-];
+]
 
 export const headerContent = {
-  title: "A lifetime of discounts? It's Genius.",
-  desc: "Get rewarded for your travels - unlock instant saving of 10% or more with a free cbooking account",
-  button: "Sign in / Register",
+  title:
+    "Trouvez des offres sur des hôtels, des hébergements indépendants et bien plus encore...",
+  desc: "Du gîte champêtre cosy à l'appartement citadin design",
+  button: "S'inscrire",
 }
+
+export const featuredsDatas = [
+  {
+    title: "Barcelone, Espagne",
+    desc: "1 304 établissements",
+    imgSrc:
+      "https://images.pexels.com/photos/6642506/pexels-photo-6642506.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    title: "Paris, France",
+    desc: "1773 établissements",
+    imgSrc:
+      "https://images.pexels.com/photos/12691629/pexels-photo-12691629.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    title: "Bruxelle, Belgique",
+    desc: "250 établissements",
+    imgSrc:
+      "https://images.pexels.com/photos/2960887/pexels-photo-2960887.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    title: "Londre, UK",
+    desc: "1050 hébergements",
+    imgSrc:
+      "https://images.pexels.com/photos/460672/pexels-photo-460672.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    title: "New York, USA",
+    desc: "+1000 hébergements",
+    imgSrc:
+      "https://images.pexels.com/photos/3043590/pexels-photo-3043590.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    title: "Rio de janeiro, Brésil",
+    desc: "+1200 hébergements",
+    imgSrc:
+      "https://images.pexels.com/photos/6048459/pexels-photo-6048459.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+]
+
+export const propertyListDatas = [
+  {
+    title: "Hotel",
+    desc: "910 927 hôtels",
+    imgSrc:
+      "https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=",
+  },
+  {
+    title: "Apartments",
+    desc: "846 953 appartements",
+    imgSrc:
+      "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg",
+  },
+  {
+    title: "Complexes hôteliers",
+    desc: "17 804 complexes hôteliers",
+    imgSrc:
+      "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg",
+  },
+  {
+    title: "Villas",
+    desc: "455 879 villas Chalets",
+    imgSrc:
+      "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg",
+  },
+  {
+    title: "Cabins",
+    desc: "2331 hotels",
+    imgSrc:
+      "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg",
+  },
+]
+export const featuredPropertyListDatas = [
+  {
+    title: "7Seasons Apartments Budapest",
+    desc: "06. Terézváros, Hongrie, Budapest",
+    price: 96,
+    stars: 8.9,
+    avis: "excellent",
+    imgSrc:
+      "https://cf.bstatic.com/xdata/images/hotel/square600/87428762.webp?k=9a065fcd92168145d8c8358701662c76793535597b678efc8f6921c8e3c188e6&o=&s=1",
+  },
+  {
+    title: "Oriente Palace Apartments",
+    desc: "Centre de Madrid, Espagne, Madrid",
+    price: 95,
+    stars: 9.3,
+    avis: "exceptionel",
+    imgSrc:
+      "https://cf.bstatic.com/xdata/images/hotel/square600/95058973.webp?k=979587fd2ac8f7777a34758264d557eef57d0e98e58bdaeb121f5b968a20f810&o=&s=1",
+  },
+  {
+    title: "numa I Vita Apartments",
+    desc: "Forteresse de Basso, Italie, Florence",
+    price: 117,
+    stars: 7.5,
+    avis: "excellent",
+    imgSrc:
+      "https://cf.bstatic.com/xdata/images/hotel/square600/352170812.webp?k=75ffc5f9eb3f3cc394b901714c1544757b05849dbbdf30e4fc8c6df53931c131&o=&s=1",
+  },
+  {
+    title: "Aparthotel Stare Miasto",
+    desc: "Centre historique, Pologne, Kraków",
+    price: 175,
+    stars: 9,
+    avis: "exceptionel",
+    imgSrc:
+      "https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1",
+  },
+]
